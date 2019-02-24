b486a3f20929fe36cbab5d531db17ed7f9f59f40
root <root@Mukul.localdomain>
2019-02-24 14:15:41 -0500

    code: Adding CLI base structure~

---
+++ CLI/cli.py
@@ -0,0 +1,23 @@
+import click
+import request
+
+SAMPLE_API_KEY = 'b1b15e88fa797225412429c1c50c122a1'
+
+def current_weather(location, api_key=SAMPLE_API_KEY):
+    url = 'http://samples.openweathermap.org/data/2.5/weather'
+
+    query_params = {
+        'q': location,
+        'appid': api_key,
+    }
+
+    response = requests.get(url, params=query_params)
+
+    return response.json()['weather'][0]['description']
+
+@click.command()
+def main():
+    print('Automated Email CLI ✨')
+
+if __name__ == "__main__":
+    main()
---
+++ CLI/install.sh
@@ -0,0 +1,2 @@
+pip3 install -e .
+pip3 uninstall -e .
