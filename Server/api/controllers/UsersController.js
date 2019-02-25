172daf7ca462d6eaa7c448b25163554836bd56c1
mukulbichkar19 <mukulbichkar@hotmail.com>
2019-02-25 17:33:35 -0500

    code: Changed the callbacks to async await wherever I can

--- Server/api/controllers/UsersController.js
+++ Server/api/controllers/UsersController.js
@@ -1,8 +1,10 @@
 'use strict';
  var mongoose = require('mongoose'), Users = mongoose.model('Users'),
  Cities = mongoose.model('Cities');
+ var Promise = require("bluebird");
 
 exports.getUsers = function(req, res){
+  console.log('inside get users from git');
   Users.find({}, function(err, users){
     if(err){
       res.send(err);
@@ -12,89 +14,68 @@ exports.getUsers = function(req, res){
   });
 };
 
- exports.createUser = function(req, res){
-   console.log('Inside createUser: ');
-   console.log(req.body.emailId);
-   console.log(req.body.city);
-   
-   var emailId = req.body.emailId;
-   var city = req.body.city;
-   var newUser = new Users(
-     {
-       emailId: emailId,
-       city: city
-     }
-   );
+async function checkIfUserExists(emailId){
+  const query = Users.findOne({emailId: emailId});
+  const user = await query.exec();
+  return user;
+}
 
+async function CheckIfCityExists(city){
+  const query = Cities.findOne({City:city});
+  return await query.exec();
+}
 
-   console.log(newUser);
-
-   var newUserId = 0;
-   // Check if user does not exists in the database
-   Users.find({ emailId: emailId}, function(err, user){
-     if(err)
-      res.send(err)
-    else {
-      if(user.length > 0){
-        res.status(403).send({message: 'User with same email address exists cannot create a new one.'})
-      }else{
-        // Create a new user
-         Users.create(newUser, function(err, user){
-           if(err){
-             res.status(500).send({message: 'Unable to create a user' + err});
-           }
-           Cities.find({City:user.city}, function(err, city){
-             if(err){
-               res.send(err);
-             }else if(city.length == 0)
-             {
-               let lcity = new Cities({ City: user.city, SubscribersList: [user._id]});
-               Cities.create(lcity, function(err, city){
-                 if(err){
-                   res.send(err);
-                 }
-                 res.status(200).send({message: 'User created successfully along with subscribers list create'});
-               });
-             }else{
-               console.log('Enters this update and push branch');
-               var subscriberList = city[0].SubscribersList;
-               subscriberList.push(user._id);
-               var CityName = city[0].City;
-               let lcity = new Cities({ City: CityName, SubscribersList: subscriberList});
-               Cities.deleteOne({City:city[0].City}, function(err, city){
-                 if(err){
-                   res.send(err);
-                 }
-                 Cities.create(lcity, function(err, city){
-                   if(err){
-                     res.send(err);
-                   }
-                   res.status(200).send({message: 'User created successfully along with updated list' + city});
-                 });
-               });
-
-               /*city[0].SubscribersList.push(user._id);
-               city.save(function(err){
-                 if(err){
-                   return res.send(err);
-                 }
-                 res.status(200).send({message: 'User created successfully along with subscribers update'+city});
-               });
-               console.log('Subscriber list after update ');
-               console.log(subscriberList);
-               Cities.updateOne({ _id: city._id}, { $set: { SubscribersList: subscriberList } },
-               function(err, city){
-                 if(err){
-                    res.send(err)
-                 }
-                 console.log(city);
-                 res.status(200).send({message: 'User created successfully along with subscribers update'+city});
-               });*/
-             }
-           });
-         });
-      }
+exports.createUser = function(req, res) {
+  let emailId = req.body.emailId;
+  let city = req.body.city;
+  let newUser = {
+    emailId: emailId,
+    city: city
+  }
+  checkIfUserExists(emailId).then(result => {
+    if(result == null){
+        // Create a user here and add him to cities SubscribersList
+        Users.create(newUser, function(err, user){
+          if(err)
+            res.status(500).send({message: 'Unable to create a user' + err});
+          CheckIfCityExists(user.city).then(city => {
+            if(city == null){
+              let lcity = new Cities({ City: user.city, SubscribersList: [user._id]});
+              Cities.create(lcity, function(err, city){
+                if(err){
+                  res.send(err);
+                }
+                res.status(200).send({message: 'User created successfully along with updated subscribers list for cities'});
+              });
+            }else{
+              var subscriberList = city.SubscribersList;
+              subscriberList.push(user._id);
+              var CityName = city.City;
+              let lcity = new Cities({ City: CityName, SubscribersList: subscriberList});
+              Cities.deleteOne({City:city.City}, function(err, city){
+                if(err){
+                  res.send(err);
+                }
+                Cities.create(lcity, function(err, city){
+                  if(err){
+                    res.send(err);
+                  }
+                  res.status(200).send({message: 'User created successfully along with updated list' + city});
+                });
+              });
+            }
+          });
+        });
+    }else{
+      res.status(403).send({message: 'User with same email address exists cannot create a new one.'});
     }
-  });
+  }).catch(error => console.log(error));
 
-};
+
+
+
+
+
+
+
+}
